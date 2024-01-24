2f3b1cd2008ad0e5df2cb85b944b784f44e4d615
nhamidi90 <nabilahhamidi@hotmail.com>
2024-01-24 12:00:20 +0000

    Fix bugs for gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,11 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+
+    //clear divs when the textbox is empty
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if(!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2`);
@@ -68,3 +73,6 @@ function fetchGitHubInformation(event) {
             }
         })
 }
+
+//have octocat profile displaying when page is loaded
+$(document).ready(fetchGitHubInformation);
